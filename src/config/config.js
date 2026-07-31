d8a2373568219df34cf0d654a58fdfc00c8fe218
arbaz-17 <126326894+arbaz-17@users.noreply.github.com>
2026-07-31 15:08:35 +0500

    Configured secret for Github page deployement

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const API_CONFIG = {
+  baseUrl: "https://api.rawg.io/api",
+  apiKey: "ea02c50908ff469a83ba8cf11d4a824c",
+  pageSize: 12,
+};
+
+export const SEARCH_CONFIG = {
+  debounceDelay: 400,
+};
